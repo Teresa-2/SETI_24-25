cd7cb80da3297fda5547fdcac15ffd5985019bff
Teresa-2 <teresa.dejesusfernandes@gmail.com>
2024-11-10 17:24:06 +0100

    test per vedere se band latency funziona

--- pingpong/pingpong/scripts/latencyBandModel.bash
+++ pingpong/pingpong/scripts/latencyBandModel.bash
@@ -1,4 +1,4 @@
-#! /bin/bash
+! /bin/bash
 # Expect protocol name as first parameter (tcp or udp)
 
 # Define input and output file names
@@ -10,11 +10,51 @@ HeadLine=($(head $ThroughFile --lines=1))
 TailLine=($(tail $ThroughFile --lines=1))
 
 #getting the information from the first and the last line
-FirstN=${HeadLine[0]}
-LastN=${TailLine[0]}
+FirstN=${HeadLine[0]} #N1
+LastN=${TailLine[0]} #N2
 
 # TO BE DONE START
+# B = (N2-N1)/(D(N2)-D(N1))
+# L0 = (D(N1)*N2 - D(N2)*N1)/(N2-N1)
+# T(N) * D(N) = N pertanto D(N) = N / T(N)
 
+{
+# DA CHIEDERE
+# Nessun controllo sul parametro passato e lo script esegue anche senza parametro UDP/TCP. è necessario inserire un controllo?
+
+# Controlla se è stato fornito un parametro
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <tcp|udp>"
+    exit 1
+fi
+
+# Assegna il parametro a una variabile
+protocol=$1
+
+# Controlla se il parametro è valido
+if [ "$protocol" != "tcp" ] && [ "$protocol" != "udp" ]; then
+    echo "Invalid parameter. Use 'tcp' or 'udp'."
+    exit 1
+fi
+
+# Define input and output file names
+ThroughFile="../data/${protocol}_throughput.dat"
+PngName="../data/LB${protocol}.png"
+}
+
+# Determination of T(N1) and T(N2)
+FirstT=${HeadLine[1]} #T(N1)
+LastT=${TailLine[1]} #T(N2)
+
+# Determination of D(N1) and D(N2)
+FirstD=$(echo "scale=10; $FirstN / $FirstT" | bc) #D(N1)
+LastD=$(echo "scale=10; $LastN / $LastT" | bc) #D(N2)
+
+# Determination of B
+Band=$(echo "scale=10; ($LastN - $FirstN) / ($LastD - $FirstD)" | bc)
+
+# Determination of L0
+Latency=$(echo "scale=10; ($FirstD * $LastN - $LastD * $FirstN) / ($LastN - $FirstN)" | bc)
 
 # TO BE DONE END
 
@@ -31,7 +71,25 @@ gnuplot <<-eNDgNUPLOTcOMMAND
   lbmodel(x)= x / ($Latency + (x/$Band))
 
 # TO BE DONE START
+  
+  # DA CHIEDERE
+  # 
+   plot lbmodel(x) title "Latency-Bandwidth model with L=$Latency and B=$Band" with linespoints, \
+       "../data/tcp_throughput.dat" using 1:2 title "TCP median Throughput" with linespoints, \
+       "../data/tcp_throughput.dat" using 1:3 title "TCP average Throughput" with linespoints, \
+       "../data/udp_throughput.dat" using 1:2 title "UDP median Throughput" with linespoints, \
+       "../data/udp_throughput.dat" using 1:3 title "UDP average Throughput" with linespoints
+  
 
+  
+  
+  
+  
+  plot lbmodel(x) title "Latency-Bandwidth model with L=$Latency and B=$Band" with linespoints, \
+       "../data/tcp_throughput.dat" using 1:2 title "TCP median Throughput" with linespoints, \
+       "../data/tcp_throughput.dat" using 1:3 title "TCP average Throughput" with linespoints, \
+       "../data/udp_throughput.dat" using 1:2 title "UDP median Throughput" with linespoints, \
+       "../data/udp_throughput.dat" using 1:3 title "UDP average Throughput" with linespoints
 # TO BE DONE END
 
   clear
