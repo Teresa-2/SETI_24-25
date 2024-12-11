cc7805f56080de09ae9b4dff11c0b093ad47c574
Teresa-2 <teresa.dejesusfernandes@gmail.com>
2024-12-11 18:44:44 +0100

    da commentare http e aux + chiedere perché non va il debug di 8.1

--- incapache/debug/test1.1.sh
+++ incapache/debug/test1.1.sh
@@ -12,16 +12,16 @@ send_request() {
 # Invia tutte le richieste HTTP al server in una sola sessione di telnet
 (
 echo -e "GET / HTTP/1.1\r\nIf-Modified-Since Mon, 11 Nov 2024 00:00:00 GMT\r\nCookie: UserID=10\r\n" #atteso: 304
-#echo -e "GET / HTTP/1.1\r\nIf-Modified-Since Mon, 11 Nov 1999 00:00:00 GMT\r\nCookie: UserID=1\r\n" #atteso: 200
+echo -e "GET / HTTP/1.1\r\nIf-Modified-Since Mon, 11 Nov 1999 00:00:00 GMT\r\nCookie: UserID=1\r\n" #atteso: 200
 #sleep $SLEEP
 echo -e "GET / HTTP/1.1\r\nCookie: UserID=16546546\r\nIf-Modified-Since: Mon, 11 Nov 2024 00:00:00 GMT\r\n"
-#echo -e "GET / HTTP/1.1\r\nIf-Modified-Since Mon, 11 Nov 1999 00:00:00 GMT\r\nCookie: UserID=1\r\n"
+echo -e "GET / HTTP/1.1\r\nIf-Modified-Since Mon, 11 Nov 1999 00:00:00 GMT\r\nCookie: UserID=1\r\n"
 
-#sleep $SLEEP
-#echo -e "GET / HTTP/1.1\r\nCookie: UserID=16\r\nIf-Modified-Since: Mon, 11 Nov 2024 00:00:00 GMT\r\n"
+sleep $SLEEP
+echo -e "GET / HTTP/1.1\r\nCookie: UserID=16\r\nIf-Modified-Since: Mon, 11 Nov 2024 00:00:00 GMT\r\n"
 #echo -e "HEAD / HTTP/1.1\r\nCookie: UserID=1\r\n"
 #sleep $SLEEP
-#echo -e "HEAD / HTTP/1.0\r\nCookie: UserID=1\r\n"
+echo -e "HEAD / HTTP/1.0\r\nCookie: UserID=1\r\n"
 #sleep $SLEEP
 ) | telnet $SERVER $PORT
 
